23547eff80c25ac168855d3e23ff80e3a9cf21ff
Wenjie <xuwenjie8866@gmail.com>
2017-10-27 19:18:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians,instruments) {
+  var array = [];
+  for (var i = 0; i < instruments.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var arrays = [];
+  i = 0
+  while ( i < facts.length) {
+    arrays.push(`${facts[i]}!!!`)
+    i++
+  }
+  return arrays;
+}
